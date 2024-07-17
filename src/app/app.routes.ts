d54eafb1e7a8ae58fc7f03c9236322c1170854d8
Jose Alejo <jtalejo@minsait.com>
2024-07-17 18:42:23 +0200

    creacion componentes y enrutados

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './features/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'user-detail',
+    loadComponent: () =>
+      import('./features/user-detail/user-detail.component').then(
+        (m) => m.UserDetailComponent
+      ),
+  },
+  {
+    path: '**',
+    redirectTo: '/',
+    pathMatch: 'full',
+  },
+];
